a4c7c1d93b99d663e007f2da1580677cc73ebd77
Tashia Davis <tashiadavis10@gmail.com>
2020-10-10 15:23:09 -0600

    Add completed order test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,32 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(order);
+  } else {
+    return deliveryOrders;
+  };
+};
+
+function refundOrder(orderNumber, deliveryOrders) {
+  deliveryOrders.splice(orderNumber - 1, 1);
+};
+
+function listItems(deliveryOrders) {
+  return `${deliveryOrders[0].item}, ${deliveryOrders[1].item}, ${deliveryOrders[2].item}`;
+};
+
+function searchOrder(deliveryOrders, searchedItem) {
+  var defaultReturn = false;
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === searchedItem) {
+    defaultReturn = true;
+    };
+  };
+  return defaultReturn;
+};
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
