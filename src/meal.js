61a549d2e0c502c47f3df73d92d09ebc8a004045
Tashia Davis <tashiadavis10@gmail.com>
2020-10-10 10:52:37 -0600

    Add completed meal test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,48 @@
-function nameMenuItem() {
+function nameMenuItem(nameMenuItem) {
+  return `Delicious ${nameMenuItem}`;
+};
 
-}
+function createMenuItem(name, price, type) {
+  var menuItemName = nameMenuItem(name);
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+};
 
+function addIngredients(topping, ingredients) {
+  if (ingredients.includes(topping, ingredients)) {
+    return ingredients;
+  } else {
+    ingredients.push(topping);
+  };
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+};
+
+function decreasePrice(initialPrice) {
+  decreasedPrice = initialPrice - (initialPrice * .10);
+  return decreasedPrice;
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  };
+  return recipe;
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
