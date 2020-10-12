d7f03e3d4ff49da99a4fdef77e0875d2cea5e7fb
Tashia Davis <tashiadavis10@gmail.com>
2020-10-11 19:47:06 -0600

    Add somewhat completed chef files

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,29 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant, menu) {
+    this.name = name;
+    this.restaurant = restaurant;
+    this.menu = [];
+  };
+  greetCustomer(customerName, morning) {
+    if (morning === true) {
+      return `Good morning, ${customerName}!`;
+    } else {
+      return `Hello, ${customerName}!`;
+    };
+  };
+  addMenuItem(foodItem) {
+    this.menu.push(foodItem);
   }
-}
+  checkForFood(foodItem) {
+    return `Yes, we're serving ${foodItem.name} today!`;
+  };
+  // checkForFood(requestedItem) {
+  //   if (requestedItem === this.menu[foodItem.name]) {
+  //     return `Yes, we're serving ${this.menu[foodItem.name]} today!`;
+  //   } else {
+  //     return `Sorry, we aren't serving ${this.menu[foodItem.name]} today.`;
+  //   };
+  // };
+};
 
 module.exports = Chef;
